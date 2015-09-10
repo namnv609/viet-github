a7068d15fe031451a43fb448623c89e77644a05f
HoangNguyen <hoang.nguyen@gsmartsolutions.com>
2015-09-10 09:32:12 +0700

    refs#43007162251083 Create migration

---
+++ database/migrations/2015_09_10_021133_create_repositories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepositoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('repositories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->text('content')->nullable();
+            $table->integer('user_id');
+            $table->integer('category_id');
+            $table->nullableTimestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('repositories');
+    }
+}
